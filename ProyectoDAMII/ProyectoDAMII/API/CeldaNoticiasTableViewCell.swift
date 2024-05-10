3739e2bd0fc4424d12d982a4d546277740ad92c8
Funky1352 <166940726+Funky1352@users.noreply.github.com>
2024-05-09 20:26:33 -0500

    ultimos cambios

---
+++ ProyectoDAMII/ProyectoDAMII/API/CeldaNoticiasTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CeldaNoticiasTableViewCell.swift
+//  ProyectoDAMII
+//
+//  Created by DAMII on 9/05/24.
+//
+
+import UIKit
+
+class CeldaNoticiasTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ProyectoDAMII/ProyectoDAMII/API/NoticiasViewController.swift
@@ -0,0 +1,85 @@
+///
+//  NoticiasViewController.swift
+//  ProyectoDAMII
+//
+//  Created by DAMII on 9/05/24.
+//
+
+import UIKit
+import SafariServices
+
+struct NoticiasModelo: Codable {
+    let articles: [Noticia]
+}
+
+struct Noticia: Codable {
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+}
+
+
+
+class NoticiasViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articulosNoticias.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tablaNoticias.dequeueReusableCell(withIdentifier: "celdaNoticia", for: indexPath)
+        celda.textLabel?.text = articulosNoticias[indexPath.row].title
+        celda.detailTextLabel?.text = articulosNoticias[indexPath.row].description
+        
+        /*if let url = URL(string: articulosNoticias[indexPath.row].urlToImage ?? ""){
+            if let imagenData = try? Data(contentsOf: url) {
+                celda.imagenNoticias.image = UIImage(data: imagenData)
+            }
+        }*/
+        return celda
+    }
+/*    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tablaNoticias.deselectRow(at: indexPath, animated: true)
+        
+        guard let urlMostrar = URL(string: articulosNoticias[indexPath.row].url ?? "") else { return }
+        let VCSS = SFSafariViewController(url: urlMostrar)
+        present(VCSS, animated: true)
+    }
+    */
+    var articulosNoticias : [Noticia] = []
+
+    @IBOutlet weak var tablaNoticias: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*tablaNoticias.register(UINib(nibName: "CeldaNoticiasTableViewCell", bundle: nil), forCellReuseIdentifier: "celdaNoticia")*/
+        
+        tablaNoticias.delegate = self
+        tablaNoticias.dataSource = self
+        
+        buscarNoticias()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func buscarNoticias() {
+        let urlString =
+            "https://newsapi.org/v2/top-headlines?apiKey=f0797ef3b62d4b90a400ed224e0f82b7&country=mx"
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                let decodificador = JSONDecoder()
+                
+                if let datosDecodificados = try?
+                    decodificador.decode(NoticiasModelo.self, from: data) {
+                    print("datosDecodificados: \(datosDecodificados.articles.count)")
+                    articulosNoticias = datosDecodificados.articles
+                    
+                    tablaNoticias.reloadData()
+                }
+            }
+        }
+    }
+    
+
+}
+
